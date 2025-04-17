64342b12fe64764c2adf6ba44cd413cbc4a1988c
Tom Aarsen <Cubiegamedev@gmail.com>
2025-04-17 10:51:24 +0200

    Incorporate all time likes into the reward for a better task distribution
    E.g. {'feature-extraction': 65,
     'fill-mask': 101,
     'sentence-similarity': 69,
     'text-classification': 84,
     'token-classification': 34,
     'translation': 26,
     'zero-shot-classification': 11}
     with a budget of 10k

--- trending_deploy/rewards.py
+++ trending_deploy/rewards.py
@@ -1,7 +1,8 @@
+import math
 from huggingface_hub import ModelInfo
 
 
 def get_reward_from_model(model: ModelInfo, task: str):
     # We can use trending score as a reward, but we can also include likes, downloads, 
     # task-specific multipliers, etc.
-    return model.trending_score
+    return model.trending_score + math.log(model.likes + 1)
