cdd51506647f38756365a634d8af23593eadab54
Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>
2025-09-15 11:29:21 +0200

    Load models by monthly downloads instead of trending score & avoid <1000 monthly downloads (#17)
    * Load models by monthly downloads instead of trending score
    
    Trending score gets 0 at some point, and then the loaded models seem to get a bit arbitrary.
    
    Also update the reward to set reward as 0 if the monthly downloads are <1000
    
    * Interleave model candidate loading from trending & downloads
    
    And ignore trending models with trending_score=0

--- trending_deploy/rewards.py
+++ trending_deploy/rewards.py
@@ -5,4 +5,6 @@ from huggingface_hub import ModelInfo
 def get_reward_from_model(model: ModelInfo, task: str):
     # We can use trending score as a reward, but we can also include likes, downloads, 
     # task-specific multipliers, etc.
+    if model.downloads < 1000:
+        return 0  # Filter out models with less than 1000 monthly downloads
     return model.trending_score + math.log(model.likes + 1)
