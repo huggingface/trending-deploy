7447d7d6fbd1e1365f7e7b004ac6768ccf50b054
Raphael Glon <oOraph@users.noreply.github.com>
2025-09-19 17:44:52 +0200

    feat: custom image, discard inference of customers when they have left
    will contribute to relieve backend with bursts of requests, increasing the latency, even long after the caller has left
    
    Signed-off-by: Raphael Glon <oOraph@users.noreply.github.com>

--- trending_deploy/deploy.py
+++ trending_deploy/deploy.py
@@ -21,7 +21,7 @@ ENDPOINT_PREFIX = "auto-"
 COLLECTION_SLUG = "hf-inference/deployed-models-680a42b770e6b6cd546c3fbc"
 DEFAULT_INSTANCE_TYPE = "intel-spr-overcommitted"
 DEFAULT_INSTANCE_SIZE = "x16"
-IMAGE = "registry.internal.huggingface.tech/hf-endpoints/inference-pytorch-cpu:api-inference-6.4.2"
+IMAGE = "registry.internal.huggingface.tech/hf-endpoints/inference-pytorch-cpu:api-inference-6.5.0"
 
 # Instance size mapping based on instance memory
 # Maps instance memory to HF instance size (x1, x2, etc.)
@@ -139,6 +139,7 @@ def deploy_model(model: Model) -> bool:
             "UNLOAD_IDLE": "true",
             "IDLE_TIMEOUT": "60",
             "IGNORE_CUSTOM_HANDLER": "true",
+            "DISCARD_LEFT": "true"
         }
         endpoint_kwargs["task"] = task
 
